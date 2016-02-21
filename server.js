b7333d5a61d0279e2884c3296b3f1752d9833a9c
Andrew Jackson <jacksonal300@gmail.com>
2016-02-21 01:15:31 -0500

    Server running

--- server.js
+++ server.js
@@ -2,15 +2,20 @@ var express = require('express');
 var youtubeNode = require('youtube-node');
 
 var app = express();
-var youTube = youtubeNode();
+var youTube = new youtubeNode();
+
+youTube.setKey('AIzaSyAPseHh9ic9P947tO6lGcAnNH0OeSp598Q');
 
 app.get('/', function(req, res) {
 	youTube.search('I\'m yours', 5, function(err, result) {
-		if(error) {
+		if(err) {
 			console.log("Error reported: " + err);
 		}else{
-			res.send(JSON.stringify(result, null, 5));
-			console.log(JSON.stringify(result, null, 5));
+			res.send(JSON.stringify(result));
 		}
 	})	
 })
+
+var port = process.env.PORT || 3000;
+console.log('Server running on port ' + port);
+app.listen(port)
