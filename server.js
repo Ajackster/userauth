7272cb7812862cf991f819e5226612f8d004cf67
root <andrewsmandrew44@gmail.com>
2016-03-07 17:29:18 -0500

    Second commit

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
 var express = require('express');
-var youtubeNode = require('youtube-node');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+var bcrypt = require('bcrypt');
+var pgQuery = require('pg-query');
+
+var salt = 10;
 
 var app = express();
-var youTube = new youtubeNode();
 
-youTube.setKey('AIzaSyAPseHh9ic9P947tO6lGcAnNH0OeSp598Q');
+var connectionString = process.env.DATABAS_URL || 'postgres://ajackster:helloworld@localhost:5432/mupetunes';
+var client = new pg.Client(connectionString);
+client.connect();
+
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', function(req, res) {
-	youTube.search('I\'m yours', 5, function(err, result) {
-		if(err) {
-			console.log("Error reported: " + err);
-		}else{
-			res.send(JSON.stringify(result));
-		}
-	})	
+	res.sendFile('index.html');
+})
+
+app.post('/login', function(req, res) {
+	var username = "\'" + req.body.username + "\'";
+	var password = req.body.password;
+	var getUserInfo = "SELECT * FROM users WHERE username=" + username;
+
+	client.query(getUserInfo, function(err, results) {
+		console.log(results.username); 
+	}) 
+})
+
+app.post('/signup', function(req, res) {
+	var username = req.body.username;
+	var password = req.body.password;
+	var hash = bcrypt.hashSync(password, salt);
+	
+	client.query('INSERT INTO users(username, password) values($1, $2)', [username, hash]);
 })
 
 var port = process.env.PORT || 3000;
